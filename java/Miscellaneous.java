c9fab314f7a8efdbc43f9e8e5c7b6b4fa6d4db57
Steven Kang <kangweibaby@163.com>
2020-08-08 12:14:19 +0800

    update get prime

--- java/Miscellaneous.java
+++ java/Miscellaneous.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Miscellaneous {
 
     public static int locate_fibonacci_diff(final int n) {
@@ -18,8 +21,35 @@ public class Miscellaneous {
         return j - n < n - i ? j - n : n - i;
     }
 
+    public static int get_prime(final int n) {
+        if (n <= 1)
+            return -1;
+
+        HashSet<Integer> primes = new HashSet<Integer>();
+
+        for (int i = 2; i <= n; i++) {
+            boolean prime = true;
+
+            for (int key : primes) {
+                if (i % key == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+            if (!prime)
+                continue;
+            primes.add(i);
+        }
+        int max = 0;
+        for (Integer integer : primes) {
+            if (max < integer)
+                max = integer;
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
-        int n = locate_fibonacci_diff(100);
+        int n = get_prime(10000);
         System.out.println(n);
     }
 }
